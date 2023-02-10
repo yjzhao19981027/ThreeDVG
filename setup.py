9ce886a1277fd424d4408f5f5a2966e3ed3251ea
yjzhao <630877986>
2023-02-10 20:18:07 +0800

    Basic information

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ from setuptools import find_packages, setup
 
 import torch
 
-PROJECT_NAME = "mmkg-mm-entity-linking"
+PROJECT_NAME = "3DVG"
 PACKAGE_NAME = PROJECT_NAME.replace("-", "_")
-DESCRIPTION = "MMKG Models"
+DESCRIPTION = "Visual Ground"
 
 TORCH_VERSION = [int(x) for x in torch.__version__.split(".")[:2]]
 assert TORCH_VERSION >= [1, 13], "Requires PyTorch >= 1.13"
 
 
 if __name__ == "__main__":
-    version = "0.1.2"
+    version = "0.1.0"
 
     print(f"Building {PROJECT_NAME}-{version}")
 
     setup(
         name=PROJECT_NAME,
         version=version,
-        author="Ming Yang",
-        author_email="ymviv@qq.com",
-        url=f"https://github.com/vivym/{PROJECT_NAME}",
-        download_url=f"https://github.com/vivym/{PROJECT_NAME}/tags",
+        author="Yinjie Zhao",
+        author_email="630877986@qq.com",
+        url=f"https://github.com/yjzhao19981027/{PROJECT_NAME}",
+        download_url=f"https://github.com/yjzhao19981027/{PROJECT_NAME}/tags",
         description=DESCRIPTION,
         long_description=Path("README.md").read_text(),
         long_description_content_type="text/markdown",
